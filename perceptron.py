fccdb6eb4f026401c16aa56ace44cf0db17c7ffb
Arseni Rynkevich <rynkevich@icloud.com>
2019-02-17 19:38:38 +0300

    Add perceptron implementation

---
+++ perceptron.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+class Perceptron:
+    CLASS_COUNT = 2
+    BIAS = 1.0
+
+    def __init__(self, vector_size, learning_rate=0.1):
+        self.vector_size = vector_size + 1
+        self.learning_rate = learning_rate
+        self._weights = np.zeros((self.CLASS_COUNT, self.vector_size))
+
+    def guess(self, vector):
+        input_vector = (self.BIAS, ) + vector
+        activation_values = self._get_activation_values(input_vector)
+        return self._activate(activation_values)
+
+    def train(self, training_data):
+        has_classification_errors = False
+        for vector, target_class in training_data:
+            input_vector = np.array((self.BIAS, ) + vector)
+            activation_values = self._get_activation_values(input_vector)
+            guess = self._activate(activation_values)
+            error = target_class - guess
+
+            if error != 0:
+                has_classification_errors = True
+
+                self._weights[target_class] += self.learning_rate * input_vector
+
+                for i in range(self.CLASS_COUNT):
+                    if i != target_class and activation_values[i] >= activation_values[target_class]:
+                        self._weights[i] -= self.learning_rate * input_vector
+
+        return has_classification_errors
+
+    def _get_activation_values(self, vector):
+        activation_values = []
+        input_vector = np.array(vector)
+        for i in range(self.CLASS_COUNT):
+            activation_values.append(self._weights[i].dot(input_vector))
+
+        return activation_values
+
+    @staticmethod
+    def _activate(activation_values):
+        return np.argmax(activation_values)
